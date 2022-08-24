6411046b23969f4e9bd891500f18bcfb235d998c
chigaevi <chigaev.i@gmail.com>
2022-08-25 00:14:07 +0700

    commit 5 (corrected after second remark by Evgeny Shmargunov)

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ class YaUploader_fromVK:
             result_dic = {'file_name': file_name, 'size': size, 'url': url_max_size_photo}
             result_list.append(result_dic)
             count += 1
-        pprint(result_list)
+        # pprint(result_list)
         result_list_copy = copy.deepcopy(result_list)
         with open('photos_info.json', 'w') as photos_info:
             for photo_info in result_list_copy:
@@ -82,27 +82,19 @@ class YaUploader_fromVK:
             print(f'file information uploaded successfully')
         else:
             print(f'file information not loaded')
-
         return result_list
 
     def upload(self):
         result_list = self._get_photos()
-
         for file_inf in result_list:
-            # print('*'*50)
-            # print(file_inf)
             photo_url = file_inf['url']
             name_file = str(file_inf['file_name']) + '.jpg'
-
             with open(name_file, 'wb') as photo_file:
                 res = requests.get(photo_url)
                 photo_file.write(res.content)
-
             path_to_file = 'photo_from_VK/' + name_file  # Загрузит в созданный с помощью функции _create_folder
-            # path_to_file = name_file #Загрузит в корень Ядиска
             temp_upload_url = self._get_upload_link(path_to_file).get('href')
             response = requests.put(temp_upload_url, data=open(name_file,'rb'))
-            # print(response.raise_for_status())
             if response.status_code == 201:
                 print(f'file "{name_file}" successfully uploaded')
             else:
@@ -110,10 +102,9 @@ class YaUploader_fromVK:
 
 if __name__ == '__main__':
     id = '708212548'
-    # id = '1'
+    id = '1'
     uploader_photo = YaUploader_fromVK(id)
     uploader_photo.upload()
-    # uploader_photo.create_folder('new_folder')
 
 
 
