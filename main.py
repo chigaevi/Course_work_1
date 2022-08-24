dd22c2cd1a44f22aec74c444c34d1f9f500712c4
chigaevi <chigaev.i@gmail.com>
2022-08-24 20:04:04 +0700

    commit 4 (corrected after second remark by Evgeny Shmargunov)

--- main.py
+++ main.py
@@ -26,6 +26,17 @@ class YaUploader_fromVK:
         #     print('we got link successfully')
         return response.json()
 
+    def _create_folder(self, new_folder_name = 'photo_from_VK'):
+        url = 'https://cloud-api.yandex.net/v1/disk/resources'
+        headers = {'Authorization': f'OAuth {self.token}'}
+        params = {'path': new_folder_name, 'overwrite': 'true'}
+        response = requests.put(url, headers=headers, params=params)
+        if response.status_code == 201:
+            print(f'folder "{new_folder_name}" created successfully')
+        else:
+            print(f'folder "{new_folder_name}" not created')
+        return new_folder_name
+
     def _get_photos(self, num = 5): # num - количество загружаемых фото
         params = {'access_token': get_tokenVK(),
                   'v': '5.131',
@@ -56,14 +67,15 @@ class YaUploader_fromVK:
             result_dic = {'file_name': file_name, 'size': size, 'url': url_max_size_photo}
             result_list.append(result_dic)
             count += 1
-        # pprint(result_list)
+        pprint(result_list)
         result_list_copy = copy.deepcopy(result_list)
         with open('photos_info.json', 'w') as photos_info:
             for photo_info in result_list_copy:
                 del photo_info['url']
                 photos_info.write(str(photo_info))
-        # path_to_file = 'photo_from_VK/photos_info.json' #Загрузит в указанный каталог Ядиска
-        path_to_file = 'photos_info.json' #Загрузит в корень Ядиска
+        self._create_folder()
+        path_to_file = 'photo_from_VK/photos_info.json' #Загрузит в созданный с помощью функции _create_folder
+        # path_to_file = 'photos_info.json' #Загрузит в корень Ядиска
         temp_upload_url = self._get_upload_link(path_to_file).get('href')
         response = requests.put(temp_upload_url, data=open('photos_info.json', 'rb'))
         if response.status_code == 201:
@@ -73,31 +85,36 @@ class YaUploader_fromVK:
 
         return result_list
 
-    def upload(self, num = 5):  # num - количество загружаемых фото
+    def upload(self):
         result_list = self._get_photos()
-        for num in range(num):  #если нужен весь список len(result_list)
-            photo_url = result_list[num]['url']
-            name_file = str(result_list[num]['file_name']) + '.jpg'
+
+        for file_inf in result_list:
+            # print('*'*50)
+            # print(file_inf)
+            photo_url = file_inf['url']
+            name_file = str(file_inf['file_name']) + '.jpg'
 
             with open(name_file, 'wb') as photo_file:
                 res = requests.get(photo_url)
                 photo_file.write(res.content)
 
-            # path_to_file = 'photo_from_VK/' + name_file #Загрузит в указанный каталог Ядиска
-            path_to_file = name_file #Загрузит в корень Ядиска
+            path_to_file = 'photo_from_VK/' + name_file  # Загрузит в созданный с помощью функции _create_folder
+            # path_to_file = name_file #Загрузит в корень Ядиска
             temp_upload_url = self._get_upload_link(path_to_file).get('href')
             response = requests.put(temp_upload_url, data=open(name_file,'rb'))
             # print(response.raise_for_status())
             if response.status_code == 201:
-                print(f'file#{num+1} successfully uploaded')
+                print(f'file "{name_file}" successfully uploaded')
             else:
-                print(f'file#{num + 1} unsuccessfully')
+                print(f'file "{name_file}" unsuccessfully')
 
 if __name__ == '__main__':
-    # id = '708212548'
-    id = '1'
+    id = '708212548'
+    # id = '1'
     uploader_photo = YaUploader_fromVK(id)
     uploader_photo.upload()
+    # uploader_photo.create_folder('new_folder')
+
 
 
 
